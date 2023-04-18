1e8ff42005abf2084d80983937924434c1fe2225
!"Nekros <122643715+Nekros-dsc@users.noreply.github.com>
2023-04-18 20:08:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const  Discord= require("discord.js")
+const client = new Discord.Client({
+    partials: ['MESSAGE', 'REACTION', 'CHANNEL'],
+    intents: Object.keys(Discord.Intents.FLAGS),
+});
+const db = require('quick.db')
+
+
+
+const salon = '' //id du salon ou il y aura les logs
+
+client.on('ready', () => {
+    console.log(`Connecté sur ${client.user.tag} !`)
+})
+
+client.on('messageDelete', async message => {
+    if (!message.embeds.toString()) return
+
+    const embed = new Discord.MessageEmbed()
+    embed.setDescription(`\\✏️ Cette embed a été supprimé dans le salon <#${message.channel.id}>`)
+    embed.setFooter({ text: 'discord.gg/novaworld' })
+    embed.setColor('2f3136')
+
+    db.set(message.channel.id, message.embeds[0].toJSON())
+
+    const button = new Discord.MessageButton()
+    button.setCustomId(message.channel.id)
+    button.setLabel('📜')
+    button.setStyle('PRIMARY')
+
+    const button2 = new Discord.MessageButton()
+    button2.setCustomId(message.channel.id)
+    button2.setLabel('📜')
+    button2.setStyle('PRIMARY')
+    button2.setDisabled(true);
+
+    const row = new Discord.MessageActionRow().addComponents([button])
+    const row2 = new Discord.MessageActionRow().addComponents([button2])
+
+    client.channels.cache.get(salon).send({ content: `Salon: <#${message.channel.id}>`, embeds: [message.embeds[0].toJSON()] })
+    client.channels.cache.get(salon).send({ embeds: [embed], components: [row] })
+
+
+    client.on('interactionCreate', async (interaction) => {
+        if (interaction.isButton()) {
+            interaction.message.edit({ components: [row2] })
+            interaction.reply({
+                ephemeral: false, embeds: [{ description: `✅ J'ai bien envoyer le message dans le salon.` }]
+            })
+            const channel = interaction.guild.channels.cache.get(interaction.customId)
+            if (channel) channel.send({ embeds: [new Discord.MessageEmbed(db.get(interaction.customId))] })
+        }
+    })
+})
+client.login('Put-Token-Here') //token
